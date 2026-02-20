29a7641c0c6104f24d45efc381223ff7fee373fa
Esekon <esekondensel@gmail.com>
2026-02-20 17:45:19 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/REPO-NAME/', 
+
   plugins: [react()],
+  
   server: {
     port: 5173,
     host: true,
@@ -13,6 +16,7 @@ export default defineConfig({
       }
     }
   },
+
   build: {
     rollupOptions: {
       output: {
